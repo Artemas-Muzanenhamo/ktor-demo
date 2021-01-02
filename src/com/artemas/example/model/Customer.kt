c584836cf5179a96c2318c93f7cebf2a806f8ef8
Artemas-Muzanenhamo <artemasseng@gmail.com>
2021-01-02 00:59:31 +0000

    Add additional routes

--- src/com/artemas/example/model/Customer.kt
+++ src/com/artemas/example/model/Customer.kt
@@ -2,7 +2,7 @@ package com.artemas.example.model
 
 import kotlinx.serialization.Serializable
 
-val customerStorage = mutableListOf<Customer>()
-
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
+
+val customerStorage = mutableListOf<Customer>()
--- src/com/artemas/example/routes/CustomerRoutes.kt
+++ src/com/artemas/example/routes/CustomerRoutes.kt
@@ -1,8 +1,13 @@
 package com.artemas.example.com.artemas.example.routes
 
+import com.artemas.example.model.Customer
 import com.artemas.example.model.customerStorage
 import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.http.HttpStatusCode.Companion.Accepted
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.http.HttpStatusCode.Companion.NotFound
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -16,13 +21,29 @@ fun Route.customerRouting() {
             }
         }
         get("{id}") {
-
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing or malformed id",
+                status = BadRequest
+            )
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = NotFound
+                )
+            call.respond(customer)
         }
         post {
-
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            call.respondText("Customer stored correctly", status = Accepted)
         }
         delete("{id}") {
-
+            val id = call.parameters["id"] ?: return@delete call.respond(BadRequest)
+            if (customerStorage.removeIf { it.id == id }) {
+                call.respondText("Customer removed correctly", status = Accepted)
+            } else {
+                call.respondText("Not Found", status = NotFound)
+            }
         }
     }
 }
