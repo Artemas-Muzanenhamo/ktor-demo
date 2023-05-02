d04d5565b3f1241299ba40444346b9a40f191197
Artemas-Muzanenhamo <artemasseng@gmail.com>
2023-05-02 12:27:32 +0100

    update dependencies

--- src/main/kotlin/com/artemas/example/Application.kt
+++ src/main/kotlin/com/artemas/example/Application.kt
@@ -1,15 +1,14 @@
 package com.artemas.example
 
 import com.artemas.example.routes.registerCustomerRoutes
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.serialization.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
